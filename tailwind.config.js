7ba0a3b2f825234b0349865dfa5983316ad8c385
Muskan Yadav <ymuskan3218@gmail.com>
2026-02-03 15:46:02 +0530

    feat: configure Tailwind CSS with dark mode support and custom variant

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // important for toggle
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
